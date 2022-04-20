a18d3dea2f9b8aba44d6dc8543c95143b8ca408b
jclegg03 <jjc2003@comcast.net>
2022-04-20 11:19:46 -0600

    Refactored the randomness to be parameterized

--- src/art/view/ShapePanel.java
+++ src/art/view/ShapePanel.java
@@ -5,16 +5,27 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
+import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 
 import art.controller.Controller;
 
 public class ShapePanel extends JPanel
 {
+	public static final int MAX_VERTICIES = 50;
+	public static final int MIN_VERTICIES = 3;
+	public static final int MAX_SCALE = 300;
+	public static final int MIN_SCALE = 10;
+	private int currentScale;
+	private int currentVerticies;
 	private Controller app;
 	private ArrayList<Polygon> triangles;
 	private ArrayList<Polygon> otherShapes;
@@ -24,22 +35,25 @@ public class ShapePanel extends JPanel
 	public ShapePanel(Controller app)
 	{
 		super();
+		this.setBackground(Color.BLACK);
+		this.currentScale = 50;
+		this.currentVerticies = 10;
 		this.app = app;
 		this.triangles = new ArrayList<Polygon>();
 		this.otherShapes = new ArrayList<Polygon>();
 		this.ellipses = new ArrayList<Ellipse2D.Double>();
 		this.canvasImage = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB);
 		
-		generateArt();
+		makeArt(10, 10, 10);
 	}
 	
 	private void generateArt()
 	{
-		Graphics2D drawingGraphics = (Graphics2D) (this.getGraphics());
+		Graphics2D drawingGraphics = (Graphics2D) (canvasImage.getGraphics());
 		
-		generateTriangles(10);
-		generateOtherShapes(10);
-		generateEllipses(10);
+		drawingGraphics.setColor(Color.BLACK);
+		drawingGraphics.setStroke(new BasicStroke(1));
+		drawingGraphics.fill(imageRectangle());
 		
 		for(Polygon triangle : triangles)
 		{
@@ -50,7 +64,7 @@ public class ShapePanel extends JPanel
 		
 		for(Polygon shape : otherShapes)
 		{
-			drawingGraphics.setStroke(new BasicStroke((int) (Math.random() * 3 + 1)));
+			drawingGraphics.setStroke(new BasicStroke((int) (Math.random() * 4 + 4)));
 			drawingGraphics.setColor(randomColor());
 			drawingGraphics.draw(shape);
 		}
@@ -61,6 +75,9 @@ public class ShapePanel extends JPanel
 			drawingGraphics.setColor(randomColor());
 			drawingGraphics.fill(ellipse);
 		}
+		
+		drawingGraphics.dispose();
+		this.repaint();
 	}
 	
 	private void generateTriangles(int numTriangles)
@@ -89,7 +106,7 @@ public class ShapePanel extends JPanel
 		
 		while(numShapes > 0)
 		{
-			int numPoints = (int) (Math.random() * 10);
+			int numPoints = (int) (Math.random() * (currentVerticies + 1));
 			
 			int[] xValues = new int[numPoints];
 			int[] yValues = new int[numPoints];
@@ -113,10 +130,10 @@ public class ShapePanel extends JPanel
 		
 		while(numEllipses > 0)
 		{
-			double xVal = (Math.random() * 560 + 20);
-			double yVal = (Math.random() * 560 + 20);
-			double length = (Math.random() * 19 + 1);
-			double width = (Math.random() * 19 + 1);
+			double xVal = (Math.random() * (600 - 2 * currentScale));
+			double yVal = (Math.random() * (600 - 2 * currentScale));
+			double length = (Math.random() * (currentScale + 1) + currentScale);
+			double width = (Math.random() * (currentScale + 1) + currentScale);
 			
 			Ellipse2D.Double ellipse = new Ellipse2D.Double(xVal, yVal, length, width);
 			ellipses.add(ellipse);
@@ -141,10 +158,64 @@ public class ShapePanel extends JPanel
 		return new Color(randRed, randGreen, randBlue);
 	}
 	
+	private Polygon imageRectangle()
+	{
+		Polygon rectangle = new Polygon();
+		
+		rectangle.addPoint(0, 0);
+		rectangle.addPoint(599, 0);
+		rectangle.addPoint(599, 599);
+		rectangle.addPoint(0, 599);
+		
+		return rectangle;
+	}
+	
 	@Override
 	protected void paintComponent(Graphics graphics)
 	{
 		super.paintComponent(graphics);
 		graphics.drawImage(canvasImage, 0, 0, null);
+	}
+	
+	public void save()
+	{
+		JFileChooser saver = new JFileChooser();
+		
+		try
+		{
+			if(saver.showSaveDialog(this) == JFileChooser.APPROVE_OPTION)
+			{
+				String savePath = saver.getSelectedFile().getPath();
+				
+				if(! savePath.endsWith(".png"))
+				{
+					savePath += ".png";
+				}
+				
+				ImageIO.write(canvasImage, "PNG", new File(savePath));
+			}
+		}
+		catch(IOException saveError)
+		{
+			app.handleError(saveError);
+		}
+		catch(NullPointerException majorError)
+		{
+			app.handleError(majorError);
+		}
+	}
+	
+	public void makeArt(int triangles, int shapes, int ellipses)
+	{
+		generateTriangles(triangles);
+		generateOtherShapes(shapes);
+		generateEllipses(ellipses);
+		
+		generateArt();
+	}
+	
+	public void clear()
+	{
+		makeArt(0, 0, 0);
 	}
 }
